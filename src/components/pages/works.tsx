351179eaca1dabfbc429309c0eac6af795d6c67b
kyami0646 <kamikami0646@gmail.com>
2022-06-13 22:09:30 +0900

    updates about, works

--- src/components/pages/works.tsx
+++ src/components/pages/works.tsx
@@ -16,7 +16,7 @@ export const Works: VFC = memo(() => {
                         <WorksCard
                         imageUrl="/image/typo.png"
                         title="国旗DEタイピング"
-                        description="国旗を覚えながらタイピング練習もできるwebアプリケーションです"
+                        description="国旗を覚えながらタイピング練習もできるwebアプリケーションです。東京オリンピックの開会式を見ている時に知らない国旗が多いことに気づいたことをきっかけに開発しました。"
                         techs={["php", "javaScript"]}
                         link="https://github.com/kyami0646/project"
                         />
@@ -25,7 +25,7 @@ export const Works: VFC = memo(() => {
                         <WorksCard
                         imageUrl="/image/studyHelp.png"
                         title="studyHelp"
-                        description="勉強の成果を視覚化したwebアプリケーションです"
+                        description="勉強の成果を視覚化したwebアプリケーションです。チーム開発で作りましたが完成はしませんでした。今度は一人で引き継ぎ完成させようと思います。"
                         techs={["Flask", "python"]}
                         link="https://github.com/kyami0646/s17"
                         />
@@ -34,9 +34,18 @@ export const Works: VFC = memo(() => {
                         <WorksCard
                         imageUrl="/image/portfolio.png"
                         title="ポートフォリオサイト"
-                        description="私の自己紹介、プロダクト紹介ページです"
+                        description="私の自己紹介、プロダクト紹介ページです。アニメーションがなく質素な作りになってしまっているので、時間ができたらつけていきたいな〜と思っています。"
                         techs={["react"]}
                         link="https://github.com/kyami0646/intro"
+                        />
+                    </WrapItem>
+                    <WrapItem  px={[6]} pb={[6]}>
+                        <WorksCard
+                        imageUrl="/image/weatherapplication.png"
+                        title="天気取得アプリ"
+                        description="APIを利用して4つの都市の転機を取得し、表示してみました。Tailwind CSSも初めて使ってみました。"
+                        techs={["react"]}
+                        link="https://github.com/kyami0646/weatherapplication"
                         />
                     </WrapItem>
                 </Wrap>
